abbebc934b7e2595c790c22f77238b352706f2a3
shopnilashik <shopnil.ashik@gmail.com>
2022-08-03 11:21:38 +0600

    New changes

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -3,17 +3,59 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageBox",
+    "sap/ui/core/Fragment",
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, JSONModel, MessageBox) {
+  function (Controller, JSONModel, MessageBox, Fragment) {
     "use strict";
 
     return Controller.extend("project1.project1.controller.View1", {
+     
       onInit: function () {
-        // let oModel = new sap.ui.model.json.JSONModel();
-        // let test = this.getView().setModel(oModel);
+       var test = getView().getModel();
+       console.log(test);
+      },
+        
+      onCreate: function (data) {
+        console.log(data);
+        var oList = this.byId("main_table");
+        var oBinding = oList.getBinding("items");
+        console.log(oBinding);
+        var oContext;
+        if(data){
+          oContext = oBinding.insertRow({
+            "id":data.id,
+            "name":data.name,
+            "description":data.description,
+            "price":data.price,
+            "stock":data.stock
+          })
+          this.pDialog.then(
+          function (oDialog) {
+            oDialog.close();
+          }
+        );
+        }else{
+          console.log("No data");
+        }
+      },
+      onSavePressed:function(){
+        var pname = this.byId("input_name");
+        var description = this.byId("input_description");
+        var price = this.byId("input_price");
+        var stock = this.byId("input_stock");
+        var name = pname.getValue();
+        description = description.getValue();
+        price = price.getValue();
+        stock = stock.getValue();
+        var buf = new Uint8Array(6);
+        window.crypto.getRandomValues(buf);
+        var id = btoa(String.fromCharCode.apply(null, buf));
+
+        this.onCreate({id,name,description,price,stock});
+        
       },
       onDeleteButtonPressed: function (oEvent) {
         let oModel = this.getView().getModel();
@@ -48,25 +90,28 @@ sap.ui.define(
           ),
         });
       },
-      onCreatePressed: function () {
-        let sPath = oEvent.getSource().getBindingContext().getPath();
-        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-        console.log(oRouter);
-        oRouter.navTo("details", {
-          path: sPath.split("/")[1],
+
+      onOpenDialog: function () {
+        if (!this.pDialog) {
+          this.pDialog = this.loadFragment({
+            name: "project1.project1.view.fragment.create",
+          });
+   
+          
+        }
+        // this.pDialog.getModel.setData(this.product);
+        this.pDialog.then(function (oDialog) {
+          oDialog.open();
         });
       },
+      onCancelPressed: function () {
+        this.pDialog.then(
+          function (oDialog) {
+            oDialog.close();
+          }.bind(this)
+        );
+      },
+     
     });
   }
 );
-// onDelete: function(oEvent) {
-//   this.mode = "delete"; var that = this;
-//   var sData = oEvent.getSource().getModel("sOrder1").getData();
-//   var oTable = this.byId("idSalesTable"); var selectedRowData = oTable.getSelectedContexts();//get the selected contexts if (selectedRowData.length === 0) {
-//   MessageToast.show("please select atleast one row"); return;
-// } else {
-//   for (var i = selectedRowData.length - 1; i >= 0; i--) {
-//     var oThisObj = selectedRowData[i].getObject(); var index = $.map(sData.Sales, function (obj, index) { if (obj === oThisObj) { return index; } });
-// 					sData.Sales.splice(index, 1);//delete  record by using Splice}
-// 				that.getView().getModel("sOrder1").setData(sData);//after deleting set the data// this._oTable.getModel().setData(sData);
-// 				oTable.removeSelections(true);}},
