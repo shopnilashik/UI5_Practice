896b94a6b65ef1539b91ec5d9497282d61e63c24
shopnilashik <shopnil.ashik@gmail.com>
2022-08-04 19:24:44 +0600

    Final

--- webapp/controller/Details.controller.js
+++ webapp/controller/Details.controller.js
@@ -1,43 +1,42 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
-  function (BaseController, History) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (BaseController, History, JSONModel) {
     "use strict";
 
     return BaseController.extend(
       "project1.project1.controller.controller.Details",
       {
         onInit() {
+          let oEditModel = new JSONModel({
+            editmode: false,
+            textMode: true,
+          });
+          this.getView().setModel(oEditModel, "editModel");
           var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
           oRouter
             .getRoute("details")
             .attachPatternMatched(this._onPatternMatched, this);
         },
         _onPatternMatched: function (oEvent) {
-          // var id = oEvent.getParameter("arguments").id;
-          // var test =   oEvent.getSource().getBindingContext().getObject();
-          // console.log(test);
-          
-         
           this.getView().bindElement({
             path:
               "/" +
               window.decodeURIComponent(oEvent.getParameter("arguments").id),
           });
+        },
+        onEditPressed: function () {
+          this._toggleEdit(true, false);
+        },
 
-          // console.log(this.getView().getModel());
-          // let test = this.getView().bindElement({
-          //   path:
-          //     "/" +
-          //     window.decodeURIComponent(oEvent.getParameter("arguments").id),
-          // });
-          // var sPath = this.getView().getModel().createKey("id", {
-          //   id: id,
-          // });
-          // console.log(sPath);
+        _toggleEdit: function (bEditMode, textMode) {
+          let oEditModel = this.getView().getModel("editModel");
 
-          // this.sCustomerPath = "/" + sPath;
-          // // console.log(this.sCustomerPath);
-          // this.getView().bindElement(this.sCustomerPath);
+          oEditModel.setProperty("/editmode", bEditMode);
+          oEditModel.setProperty("/textMode", textMode);
         },
         onNavButton: function () {
           var oHistory = History.getInstance();
@@ -50,6 +49,48 @@ sap.ui.define(
             oRouter.navTo("App", {}, true);
           }
         },
+        onSavePressed: function () {
+          var _id = this.byId("input_id");
+          var _name = this.byId("input_name");
+          var _description = this.byId("input_description");
+          var _price = this.byId("input_price");
+          var _stock = this.byId("input_stock");
+          var name = _name.getValue();
+          var description = _description.getValue();
+          var price = _price.getValue();
+          var stock = _stock.getValue();
+          var id = _id.getValue();
+          var newData = {
+            id: id,
+            name: name,
+            description: description,
+            price: price,
+            stock: stock,
+          };
+          var aModel = this.getOwnerComponent().getModel().getData();
+          for (let i = 0; i < aModel.products.length; i++) {
+            var temp = aModel.products[i];
+            if (temp.id === id) {
+              temp = newData;
+              aModel.products[i] = temp;
+              break;
+            }
+          }
+
+          var oHistory = History.getInstance();
+          var sPreviousHash = oHistory.getPreviousHash();
+
+          if (sPreviousHash !== undefined) {
+            window.history.go(-1);
+          } else {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("App", {}, true);
+          }
+          this._toggleEdit(false, true);
+        },
+        onCancelPressed: function () { 
+          this._toggleEdit(false, true);
+        }
       }
     );
   }
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -12,36 +12,32 @@ sap.ui.define(
     "use strict";
 
     return Controller.extend("project1.project1.controller.View1", {
-     
       onInit: function () {
-      let oEditModel = new JSONModel({
-                    editBtn: true,
-                    saveBtn: false 
-                });
-            
+        let oEditModel = new JSONModel({
+          editmode: false,
+        });
+
         this.getView().setModel(oEditModel, "editModel");
-        this.oData = new JSONModel(
-          {
-            "id": "",
-            "name": "",
-            "description": "",
-            "price": "",
-            "stock": ""
-          });
-          this.getView().setModel(this.oData, "editDataModel");
+        this.oData = new JSONModel({
+          id: "",
+          name: "",
+          description: "",
+          price: "",
+          stock: "",
+        });
+        this.getView().setModel(this.oData, "editDataModel");
       },
       // onEditPressed: function(){
       //       this._toggleEdit(true);
       //   },
-        
-        _toggleEdit: function(editStn,saveStn){
-            let oEditModel = this.getView().getModel("editModel");
-        
-            oEditModel.setProperty("/editBtn", editStn);
-            oEditModel.setProperty("/saveBtn", saveStn);
-  
-        },
-        
+
+      _toggleEdit: function (editStn, saveStn) {
+        let oEditModel = this.getView().getModel("editModel");
+
+        oEditModel.setProperty("/editBtn", editStn);
+        oEditModel.setProperty("/saveBtn", saveStn);
+      },
+
       onCreate: function (data) {
         console.log(data);
         var oList = this.byId("main_table");
@@ -49,41 +45,46 @@ sap.ui.define(
         console.log(oBinding);
         var oContext;
         var _data = {
-            "id":data.id,
-            "name":data.name,
-            "description":data.description,
-            "price":data.price,
-            "stock":data.stock
-          }
-          var oModel =  this.getOwnerComponent().getModel().getData();
-          console.log(oModel);
-        if(data){
+          id: data.id,
+          name: data.name,
+          description: data.description,
+          price: data.price,
+          stock: data.stock,
+        };
+        var oModel = this.getOwnerComponent().getModel().getData();
+        console.log(oModel);
+        if (data) {
           oModel.products.push(_data);
           this.getOwnerComponent().getModel().setData(oModel);
-          this.pDialog.then(
-          function (oDialog) {
+          console.log(oModel);
+          this.pDialog.then(function (oDialog) {
             oDialog.close();
-          }
-        );
-        }else{
+          });
+        } else {
           console.log("No data");
         }
+       
       },
-      onSavePressed:function(){
-        var pname = this.byId("input_name");
-        var description = this.byId("input_description");
-        var price = this.byId("input_price");
-        var stock = this.byId("input_stock");
-        var name = pname.getValue();
-        description = description.getValue();
-        price = price.getValue();
-        stock = stock.getValue();
+      
+      onSavePressed: function () {
+        var _name = this.byId("input_name");
+        var _description = this.byId("input_description");
+        var _price = this.byId("input_price");
+        var _stock = this.byId("input_stock");
+        var name = _name.getValue();
+        var description = _description.getValue();
+        var price = _price.getValue();
+        var stock = _stock.getValue();
         var buf = new Uint8Array(6);
         window.crypto.getRandomValues(buf);
         var id = btoa(String.fromCharCode.apply(null, buf));
 
-        this.onCreate({id,name,description,price,stock});
-        
+        this.onCreate({ id, name, description, price, stock });
+         id = "";
+        _name.setValue("");
+        _description.setValue("");
+        _price.setValue("");
+        _stock.setValue("");
       },
       onDeleteButtonPressed: function (oEvent) {
         let oModel = this.getView().getModel();
@@ -91,21 +92,40 @@ sap.ui.define(
           .getModel("i18n")
           .getResourceBundle();
         let oSource = oEvent.getSource();
-        let sPath = oSource.getBindingContext().getPath();
-        // oModel.remove(sPath);
-        var row = oEvent.getSource().getParent().getId();
-        var parent = oEvent.getSource().getParent().getParent();
-
-        MessageBox.warning(oResourceBundle.getText("sureToDeleteQuestion"), {
+        var test = oSource.getBindingContext().getObject();
+        console.log(test.id);
+        // var row = oEvent.getSource().getParent().getId();
+        // var parent = oEvent.getSource().getParent().getParent();
+    
+        
+         MessageBox.warning(oResourceBundle.getText("sureToDeleteQuestion"), {
           title: oResourceBundle.getText("sureToDeleteTitle"),
           actions: [MessageBox.Action.YES, MessageBox.Action.NO],
           emphasizedAction: MessageBox.Action.YES,
           onClose: (oAction) => {
             if (MessageBox.Action.YES === oAction) {
-              parent.removeItem(row);
+              this.oRowData = oSource.getBindingContext().getObject();
+              console.log(this.oRowData);
+                  var aModel = this.getOwnerComponent().getModel().getData();
+                  let index = 1;
+                  console.log(aModel);
+                  for (let i = 0; i < aModel.products.length; i++) {
+                    var temp = aModel.products[i];
+                    if (temp.id === this.oRowData.id) {
+                      index = i;
+                      console.log(i, temp.id);
+                      temp = ""; 
+                      break;
+                    }
+                  }
+              console.log(index);
+            aModel.products.splice(index, 1);
+            console.log(aModel);
+              this.getOwnerComponent().getModel().refresh();
             }
           },
-        });
+         });
+        
       },
       onListItemPressed: function (oEvent) {
         let sPath = oEvent.getSource().getBindingContext().getPath();
@@ -120,13 +140,20 @@ sap.ui.define(
       },
 
       onOpenDialog: function () {
+         var _name = this.byId("input_name");
+         var _description = this.byId("input_description");
+         var _price = this.byId("input_price");
+        var _stock = this.byId("input_stock");
+        //  _name.setValue("");
+        // _description.setValue("");
+        // _price.setValue("");
+        // _stock.setValue("");
         if (!this.pDialog) {
           this.pDialog = this.loadFragment({
             name: "project1.project1.view.fragment.create",
           });
-          
         }
-      
+
         // this.pDialog.getModel.setData(this.product);
         this.pDialog.then(function (oDialog) {
           oDialog.open();
@@ -136,57 +163,59 @@ sap.ui.define(
         this.pDialog.then(
           function (oDialog) {
             oDialog.close();
+            this.getView().getModel("editDataModel").setData(this.oData);
           }.bind(this)
         );
       },
-      onEditButtonPressed:function(oEvent){
-        //   let sPath = oEvent.getSource().getBindingContext().getPath();
-        // var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-        // var oItem = oEvent.getSource();
-        // // console.log(oItem.getBindingContext().getObject().id);
-        // oRouter.navTo("details", {
-        //   id: window.encodeURIComponent(
-        //     oItem.getBindingContext().getPath().substr(1)
-        //   ),
-        // });
-  
-        // var pname = this.byId("input_name").setText(val);
-        // var description = this.byId("input_description");
-        // var price = this.byId("input_price");
-        // var stock = this.byId("input_stock");
-        // var name = pname.getValue();
-        // description = description.getValue();
-        // price = price.getValue();
-        // stock = stock.getValue();
-        let oRowData = oEvent.getSource().getBindingContext().getObject();
-        console.log(oRowData);
-        
-        // this._toggleEdit(false,true);
-       if (!this.pDialog) {
-          this.pDialog = this.loadFragment({
-            name: "project1.project1.view.fragment.change",
-          });
-          
-        }
-        // var test = this.pDialog.getModel("editDataModel").setData(oRowData);
-      let oModel = this.getView().getModel("editDataModel").setData(oRowData);
-      console.log(oModel);
-        // this.pDialog.getModel.setData(this.product);
-        this.pDialog.then(function (oDialog) {
-            
-          // this.getView().addDependent(oDialog);
-          // oDialog.getModel("editDataModel").setModel(this.oData);
 
-        
-    
-  
-          oDialog.open();
-        });
-      },
-      _onEditSavePressed:function(){
-        this._toggleEdit(true,false);
-      }
-        
+      // onEditButtonPressed: function (oEvent) {
+      //   this.oRowData = oEvent.getSource().getBindingContext().getObject();
+      //   console.log(this.oRowData);
+
+      //   if (!this.pDialog) {
+      //     this.pDialog = this.loadFragment({
+      //       name: "project1.project1.view.fragment.change",
+      //     });
+      //   }
+      //   this.getView().getModel("editDataModel").setData(this.oRowData);
+      //   this.pDialog.then(function (oDialog) {
+      //     oDialog.open();
+      //   });
+      // },
+      // onUpdateBtnPress: function () {
+      //   var name = this.byId("input_name");
+      //   var description = this.byId("input_description");
+      //   var price = this.byId("input_price");
+      //   var stock = this.byId("input_stock");
+      //   name = name.getValue();
+      //   description = description.getValue();
+      //   price = price.getValue();
+      //   stock = stock.getValue();
+      //   var newData = {
+      //     id: this.oRowData.id,
+      //     name: name,
+      //     description: description,
+      //     price: price,
+      //     stock: stock,
+      //   };
+      //   var aModel = this.getOwnerComponent().getModel().getData();
+      //   // console.log(aModel);
+      //   console.log(aModel.products.length);
+      //   for (let i = 0; i < aModel.products.length; i++) {
+      //     var temp = aModel.products[i];
+      //     if (temp.id === this.oRowData.id) {
+      //       temp = newData;
+      //       aModel.products[i] = temp;
+      //       break;
+      //     }
+      //   }
+      //   this.getOwnerComponent().getModel().setData(aModel);
+      //    this.pDialog.then(
+      //     function (oDialog) {
+      //       oDialog.close();
+      //     }.bind(this)
+      //   );
+      // },
     });
   }
 );
